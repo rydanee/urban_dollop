ceb5b5fcfe8f3d7d66abb5148c23571d5dfbaa2d
rydanee <rydanee1@gmail.com>
2025-12-19 11:52:55 +0500

    hz

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <math.h>
+
+using namespace std;
+
+float dist(float x1, float y1, float x2, float y2) {
+    float distance = sqrt(pow(abs(x2 - x1), 2) + pow(abs(y2 - y1), 2));
+    return distance; 
+}
 
 int main() {
-    std::cout << 10 << std::endl;
+    cout << dist(1, 1, 8, 8) << endl;
 }
